7967d09a35771308c888402028f962c5f7c6de1d
Adrian-REH <adrianherrera.r.e@gmail.com>
2024-12-14 17:48:58 +0100

    refactor delete functions unused

--- cpp-06/ex00/main.cpp
+++ cpp-06/ex00/main.cpp
@@ -15,18 +15,6 @@
 #include <sstream>
 
 
-
-
-
-int strlen(char* str) {
-	int i = 0;
-
-	if (!str)
-		return 0;
-	while (str[i])
-		i++;
-	return i;
-}
 std::string arrToStr(char *val)
 {
 	int i = -1;
@@ -40,30 +28,6 @@ std::string arrToStr(char *val)
 #include <iostream>
 #include <limits>
  
-/* int main() 
-{
-    std::cout << "type\t│ lowest()\t│ min()\t\t│ max()\n"
-              << "bool\t│ "
-              << std::numeric_limits<bool>::lowest() << "\t\t│ "
-              << std::numeric_limits<bool>::min() << "\t\t│ "
-              << std::numeric_limits<bool>::max() << '\n'
-              << "uchar\t│ "
-              << +std::numeric_limits<unsigned char>::lowest() << "\t\t│ "
-              << +std::numeric_limits<unsigned char>::min() << "\t\t│ "
-              << +std::numeric_limits<unsigned char>::max() << '\n'
-              << "int\t│ "
-              << std::numeric_limits<int>::lowest() << "\t│ "
-              << std::numeric_limits<int>::min() << "\t│ "
-              << std::numeric_limits<int>::max() << '\n'
-              << "float\t│ "
-              << std::numeric_limits<float>::lowest() << "\t│ "
-              << std::numeric_limits<float>::min() << "\t│ "
-              << std::numeric_limits<float>::max() << '\n'
-              << "double\t│ "
-              << std::numeric_limits<double>::lowest() << "\t│ "
-              << std::numeric_limits<double>::min() << "\t│ "
-              << std::numeric_limits<double>::max() << '\n';
-} */
 int main(int argc, char **argv) {
 	
 	if (argc != 2) {
