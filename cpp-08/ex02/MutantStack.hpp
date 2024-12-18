e2f6d38b7254119b81c47fbf4c4d83c1ecfb6117
Adrian Herrera <adrianherrera.r.e@gmail.com>
2024-12-18 19:17:29 +0100

    add coverage code

--- cpp-08/ex02/MutantStack.hpp
+++ cpp-08/ex02/MutantStack.hpp
@@ -1,24 +1,51 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adherrer <adherrer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 17:40:06 by adherrer          #+#    #+#             */
+/*   Updated: 2024/12/18 18:34:03 by adherrer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 #include <deque>
 #include <stack>
+#include <iostream>
 
 template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T>{
 	public:
-		MutantStack();
-		MutantStack(T);
-		~MutantStack();
-		MutantStack(MutantStack const &other);
-		MutantStack &operator=(MutantStack const &other);
+		MutantStack() : std::stack<T>() {}
+		MutantStack(T src) : std::stack<T>(std::deque<T>(src)) {}
+		~MutantStack() {}
+		MutantStack(MutantStack const &other) : std::stack<T>(other) {}
+		MutantStack &operator=(MutantStack const &other) {
+			if (this != &other) {
+				std::stack<T>::operator=(other);
+			}
+			return *this;
+		}
 		typedef typename Container::iterator iterator;
 		typedef typename Container::const_iterator const_iterator;
-		iterator begin();
-		iterator end();
-		const_iterator begin() const;
-		const_iterator end() const;
+		iterator begin() {
+			return this->c.begin();
+		}
+
+		iterator end() {
+			return this->c.end();
+		}
+
+		const_iterator begin() const {
+			return this->c.begin();
+		}
+
+		const_iterator end() const {
+			return this->c.end();
+		}
 };
 
 #endif
